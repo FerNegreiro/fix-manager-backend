d6cc4b9ae9107b3dd75a5981b3c131af4f6a9bf6
FerNegreiro <fernandoivo08@hotmail.com>
2026-05-23 20:53:52 -0300

    Refatorando a configuração do banco de dados para remover a carga desnecessária do dotenv

--- app/database.py
+++ app/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-
-load_dotenv()
-
+# ISSO AQUI PEGA A URL QUE VOCÊ COLOCOU NO PAINEL DO RENDER
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL, 
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
